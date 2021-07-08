470befe66a50709bc91c54ed1e100bacdb01801f
Nikhil Rawat <nikhil.rawat786.nr@gmail.com>
2021-07-09 00:44:45 +0530

    Added ConnectionStrings

--- OnlineMovieBooking/OnlineMovieBooking/Models/Show.cs
+++ OnlineMovieBooking/OnlineMovieBooking/Models/Show.cs
@@ -10,7 +10,7 @@ namespace OnlineMovieBooking.Models
         public int ShowId { get; set; }
         public DateTime Date { get; set; }
         public DateTime StartTime { get; set; }
-        public DateTime dateTime { get; set; }
+        public DateTime EndTime { get; set; }
         public int CinemaHallId { get; set; }
         public int MovieId { get; set; }
     }
